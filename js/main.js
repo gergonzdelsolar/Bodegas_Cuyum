c9bc63eb5b9763ecc8886afa20c8658f9d7bc122
Matias Sendra <mjs.sendra@gmail.com>
2023-05-15 02:18:26 -0300

    cambios en main.js: envio de email completado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const btn = document.getElementById('button');
 
 
 const nombre = document.getElementById("nombre");
@@ -5,22 +6,23 @@ const apellido = document.getElementById("apellido");
 const email = document.getElementById("email");
 const fecha = document.getElementById("fecha");
 const horario = document.getElementById("horario");
-
 const form = document.getElementById("formulario");
-
 const parrafo = document.getElementById("warnings");
 
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
+form.addEventListener('submit', e => {
+    event.preventDefault();
 
+
+    /*======================================================================*/
+    /*   COMIENZO DE PARTE DE VALIDACION DE FORMULARIO */
     let warnings = "";
     let entrar = false;
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
 
     parrafo.innerHTML = ""
 
-    console.log(nombre.value.trim().length);
+    //console.log(nombre.value.trim().length);
 
     if (nombre.value.length == 0) {
         warnings += `Falta introducir nombre <br>`;
@@ -38,10 +40,7 @@ form.addEventListener("submit", e => {
                 break;
             }
         }
-
     }
-
-
 
     if (apellido.value.length == 0) {
         warnings += `Falta introducir apellido <br>`;
@@ -49,7 +48,7 @@ form.addEventListener("submit", e => {
     } else if (apellido.value.length < 3) {
         warnings += `El apellido no es válido <br>`;
         entrar = true;
-    }else if (apellido.value.length >= 3) {
+    } else if (apellido.value.length >= 3) {
 
         for (var i = 0; i < apellido.value.trim().length; i++) {
             var charCode = apellido.value.charCodeAt(i);
@@ -61,8 +60,6 @@ form.addEventListener("submit", e => {
         }
 
     }
-
-
 
     if (email.value.length == 0) {
         warnings += `Falta introducir email <br>`;
@@ -84,8 +81,46 @@ form.addEventListener("submit", e => {
 
 
     if (entrar) {
+        // SI HAY ERRORES SE MUESTRAN EN PANTALLA
+        parrafo.style = "color: red";
         parrafo.innerHTML = warnings
+        setTimeout(() => {
+            parrafo.innerHTML = "";
+        }, 6000);
     } else {
-        parrafo.innerHTML = "Solicitud de reserva enviada correctamente, en breve recibirá un email de confirmación en su correo electrónico."
+        // SI NO HAY ERRORES SALE EL CARTEL EN VERDE Y LUEGO ACTIVA EL ENVIO AL BUZON DE CORREO....
+
+        parrafo.style = "color: green";
+        parrafo.innerHTML = "Solicitud de reserva enviada correctamente, en breve recibirá un email de confirmación en su correo electrónico. "
+
+        /*  FIN VALIDACION DE FORMULARIO */
+        /*======================================================================*/
+
+        /* DESDE ACA EMPIEZA LA PARTE QUE ENVIA EL MAIL */
+
+        btn.value = 'Enviando...';
+        setTimeout(() => {
+            parrafo.innerHTML = "";
+        }, 10000);
+
+
+
+        const serviceID = 'default_service';
+        const templateID = 'template_6f2o81s';
+
+
+        emailjs.sendForm(serviceID, templateID, form)
+            .then(() => {
+                btn.value = 'Solicitar Reserva';
+                //alert('Mensaje enviado correctamente!');
+
+
+
+            }, (err) => {
+                btn.value = 'Solicitar Reserva';
+                alert(JSON.stringify(err));
+            });
+
     }
+
 });
